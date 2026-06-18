b5c4ec1acc43565d1214a045c1f71833c50df442
mohteguhh <mohteguhh@gmail.com>
2026-06-19 04:37:16 +0700

    Update Kasir POS: 19/06/2026  4.37.16,73

--- app.js
+++ app.js
@@ -877,7 +877,17 @@ function openCameraScanner() {
     { facingMode: "environment" },
     {
       fps: 10,
-      qrbox: { width: 250, height: 160 } // Bidik kode barcode horizontal
+      qrbox: { width: 250, height: 160 }, // Bidik kode barcode horizontal
+      formatsToSupport: [
+        Html5QrcodeSupportedFormats.EAN_13,
+        Html5QrcodeSupportedFormats.EAN_8,
+        Html5QrcodeSupportedFormats.CODE_39,
+        Html5QrcodeSupportedFormats.CODE_93,
+        Html5QrcodeSupportedFormats.CODE_128,
+        Html5QrcodeSupportedFormats.UPC_A,
+        Html5QrcodeSupportedFormats.UPC_E,
+        Html5QrcodeSupportedFormats.ITF
+      ]
     },
     onScanSuccess,
     onScanFailure
